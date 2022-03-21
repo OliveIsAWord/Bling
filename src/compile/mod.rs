d5f8919fea640be63644c8173a787b7278fb71ce
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2022-03-20 23:03:38 -0700

    Added blocks and scopes

--- src/compile/mod.rs
+++ src/compile/mod.rs
@@ -3,83 +3,131 @@
 use crate::parse::{Expr, Ident};
 
 /// Bytecode operations.
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Op {
     GetConstant(usize),
-    GetIdent(usize),
-    Assign(usize),
-    Declare(usize),
-    //Call,
+    GetIdent(Ident),
+    Assign(Ident),
+    Declare(Ident),
+    Call(usize),
 }
 
 #[derive(Debug, Clone)]
 pub enum Value {
-    // None,
+    None,
     Number(i64),
 }
 
 /// Executable bytecode environment.
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 pub struct Code {
     pub ops: Vec<Op>,
     pub idents: Vec<Ident>,
     pub constants: Vec<Value>,
+    pub codes: Vec<Code>, // FIXME: Better name
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Return {
+    Discard,
+    Keep,
+}
+
+impl Return {
+    pub fn keep(self) -> bool {
+        self == Self::Keep
+    }
 }
 
 impl Code {
-    fn add_expr(&mut self, expr: Expr, returns_value: bool) {
+    fn add_expr(&mut self, expr: Expr, returns: Return) {
         match expr {
             Expr::Number(val) => {
-                if returns_value {
+                if returns.keep() {
                     self.constants.push(Value::Number(val));
                     let index = self.constants.len() - 1;
                     self.ops.push(Op::GetConstant(index));
                 }
             }
             Expr::Identifier(name) => {
-                if returns_value {
-                    let index = self.add_ident(name);
-                    self.ops.push(Op::GetIdent(index));
+                if returns.keep() {
+                    //let index = self.add_ident(name);
+                    //self.ops.push(Op::GetIdent(index));
+                    self.ops.push(Op::GetIdent(name));
                 }
             }
             Expr::Assignment(lhs, rhs) => {
-                self.add_expr(*rhs, true);
-                let index = self.add_ident(lhs);
-                self.ops.push(Op::Assign(index));
-                if returns_value {
-                    self.ops.push(Op::GetIdent(index));
+                // self.add_expr(*rhs, Return::Keep);
+                // let index = self.add_ident(lhs);
+                // self.ops.push(Op::Assign(index));
+                // if returns.keep() {
+                //     self.ops.push(Op::GetIdent(index));
+                // }
+                self.add_expr(*rhs, Return::Keep);
+                self.ops.push(Op::Assign(lhs.clone()));
+                if returns.keep() {
+                    self.ops.push(Op::GetIdent(lhs));
                 }
             }
             Expr::Declaration(lhs, rhs) => {
-                self.add_expr(*rhs, true);
-                let index = self.add_ident(lhs);
-                self.ops.push(Op::Declare(index));
-                if returns_value {
-                    self.ops.push(Op::GetIdent(index));
+                // self.add_expr(*rhs, Return::Keep);
+                // let index = self.add_ident(lhs);
+                // self.ops.push(Op::Declare(index));
+                // if returns.keep() {
+                //     self.ops.push(Op::GetIdent(index));
+                // }
+                self.add_expr(*rhs, Return::Keep);
+                self.ops.push(Op::Declare(lhs.clone()));
+                if returns.keep() {
+                    self.ops.push(Op::GetIdent(lhs));
                 }
             }
-            // Expr::Block(exprs) => {
-            //     let (last_expr, body) = exprs.split_last();
-            // }
+            Expr::Block(exprs) => {
+                // if let Some(last_expr) = exprs.pop() {
+                //     for body_expr in exprs {
+                //         self.add_expr(body_expr, Return::Discard);
+                //     }
+                //     self.add_expr(last_expr, returns_value);
+                // } else if returns.keep() {
+                //     self.constants.push(Value::None);
+                //     let index = self.constants.len() - 1;
+                //     self.ops.push(Op::GetConstant(index));
+                // }
+                let code = Self::compile(exprs, returns);
+                self.codes.push(code);
+                let index = self.codes.len() - 1;
+                self.ops.push(Op::Call(index));
+            }
             _ => todo!(),
         }
     }
-    fn add_ident(&mut self, name: Ident) -> usize {
-        self.idents
-            .iter()
-            .position(|x| x == &name)
-            .unwrap_or_else(|| {
-                self.idents.push(name);
-                self.idents.len() - 1
-            })
+
+    fn compile(mut exprs: Vec<Expr>, returns: Return) -> Self {
+        let mut code = Self::default();
+        if let Some(last_expr) = exprs.pop() {
+            for expr in exprs {
+                code.add_expr(expr, Return::Discard);
+            }
+            code.add_expr(last_expr, returns);
+        } else if returns.keep() {
+            code.constants = vec![Value::None];
+            code.ops = vec![Op::GetConstant(0)];
+        }
+        code
     }
+
+    // fn add_ident(&mut self, name: Ident) -> usize {
+    //     self.idents
+    //         .iter()
+    //         .position(|x| x == &name)
+    //         .unwrap_or_else(|| {
+    //             self.idents.push(name);
+    //             self.idents.len() - 1
+    //         })
+    // }
 }
 
 /// Compiles a series of [Expr]s into a [Code] object.
 pub fn compile(exprs: Vec<Expr>) -> Code {
-    let mut code = Code::default();
-    for expr in exprs {
-        code.add_expr(expr, false);
-    }
-    code
+    Code::compile(exprs, Return::Discard)
 }
--- src/interpret/mod.rs
+++ src/interpret/mod.rs
@@ -1,4 +1,5 @@
 use crate::compile::{Code, Op, Value};
+use crate::parse::Ident;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::mem;
@@ -6,9 +7,19 @@ use std::mem;
 #[derive(Debug, Default)]
 pub struct Executor {
     code: Code,
-    scope: HashMap<usize, Value>,
+    op_pointer: usize,
+    scope: HashMap<Ident, Value>,
     stack: Vec<Value>,
-    parent: Option<Box<Executor>>,
+    parent: Option<(Box<Executor>, usize)>,
+}
+
+enum InternalError {
+    StackUnderflow,
+    StackOverflow,
+}
+enum ScriptError {
+    VariableNotFound,
+    VariableRedeclared,
 }
 
 impl Executor {
@@ -20,14 +31,21 @@ impl Executor {
     }
 
     pub fn run(&mut self) {
-        // Take `ops` in this scope to prevent simultaneous mutable references.
-        let ops = mem::take(&mut self.code.ops);
-        for &op in &ops {
-            self.run_step(op);
+        // Access `ops` by index to prevent simultaneous mutable references.
+        // This could also be done with mem::take to the same effect.
+        loop {
+            if let Some(op) = self.code.ops.get(self.op_pointer) {
+                let op = op.clone();
+                self.op_pointer += 1;
+                println!("awa {:?}", self);
+                println!("using op {:?}", op);
+                self.run_step(op);
+            } else if self.parent.is_some() {
+                self.exit_subroutine();
+            } else {
+                break;
+            }
         }
-        // It is incorrect behavior for the interpreter to modify the bytecode.
-        assert_eq!(self.code.ops, vec![]);
-        self.code.ops = ops;
     }
 
     fn run_step(&mut self, op: Op) {
@@ -36,23 +54,23 @@ impl Executor {
                 let val = self.code.constants.get(val_index).unwrap();
                 self.stack.push(val.clone());
             }
-            Op::GetIdent(ident_index) => {
-                let val = self.scope.get(&ident_index).unwrap();
-                self.stack.push(val.clone());
+            Op::GetIdent(ident) => {
+                let val = self.lookup_value(ident).clone();
+                self.stack.push(val);
             }
             // Op::Dup => {
             //     let val = self.stack.last().unwrap().clone();
             //     self.stack.push(val);
             // }
-            Op::Declare(ident_index) => {
+            Op::Declare(ident) => {
                 let value = self.stack.pop().unwrap();
-                match self.scope.entry(ident_index) {
+                match self.scope.entry(ident.clone()) {
                     Entry::Occupied(entry) => {
-                        let ident_name = self.code.idents.get(ident_index).unwrap();
+                        //let ident_name = self.code.idents.get(ident_index).unwrap();
                         let existing_value = entry.get();
                         panic!(
                             "Attempted to declare variable {} with value {:?} but was already assigned with value of {:?}",
-                            ident_name,
+                            ident,
                             value,
                             existing_value,
                         );
@@ -62,34 +80,64 @@ impl Executor {
                     }
                 }
             }
-            Op::Assign(ident_index) => {
+            Op::Assign(ident) => {
                 let value = self.stack.pop().unwrap();
-                match self.scope.entry(ident_index) {
-                    Entry::Occupied(mut entry) => {
-                        entry.insert(value);
-                    }
-                    Entry::Vacant(_) => {
-                        let ident_name = self.code.idents.get(ident_index).unwrap();
-                        panic!(
-                            "Attempted to assign variable {} before it was declared",
-                            ident_name,
-                        );
-                    }
-                }
+                *self.lookup_value_mut(ident) = value;
+                // match self.scope.entry(ident.clone()) {
+                //     Entry::Occupied(mut entry) => {
+                //         entry.insert(value);
+                //     }
+                //     Entry::Vacant(_) => {
+                //         //let ident_name = self.code.idents.get(ident_index).unwrap();
+                //         panic!(
+                //             "Attempted to assign variable {} before it was declared",
+                //             ident,
+                //         );
+                //     }
+                // }
+            }
+            Op::Call(code_index) => {
+                let block_code = self.code.codes.get(code_index).unwrap().clone();
+                self.enter_subroutine(block_code);
             }
         }
     }
 
+    fn lookup_value(&self, name: Ident) -> &Value {
+        if let Some(val) = self.scope.get(&name) {
+            val
+        } else if let Some(parent) = &self.parent {
+            parent.0.lookup_value(name)
+        } else {
+            panic!("Could not find variable {}", name)
+        }
+    }
+
+    fn lookup_value_mut(&mut self, name: Ident) -> &mut Value {
+        if let Some(val) = self.scope.get_mut(&name) {
+            val
+        } else if let Some(parent) = &mut self.parent {
+            parent.0.lookup_value_mut(name)
+        } else {
+            panic!("Could not find variable {}", name)
+        }
+    }
+
     fn enter_subroutine(&mut self, routine: Code) {
+        let ptr = self.op_pointer;
         let child = Self::from_code(routine);
         let mut parent = mem::replace(self, child);
+        //self.code = mem::take(&mut parent.code);
         self.stack = mem::take(&mut parent.stack);
-        self.parent = Some(Box::new(parent));
+        self.parent = Some((Box::new(parent), ptr));
+        self.op_pointer = 0;
     }
 
     fn exit_subroutine(&mut self) {
-        let parent = mem::take(&mut self.parent).unwrap();
+        let (parent, ptr) = mem::take(&mut self.parent).unwrap();
         let child = mem::replace(self, *parent);
+        //self.code = child.code;
         self.stack = child.stack;
+        self.op_pointer = ptr;
     }
 }
