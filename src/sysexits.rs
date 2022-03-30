75d22d41d11a4bf0a95dd88f1d770d5a7eab729e
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2022-03-30 03:17:44 -0700

    Create sysexits.rs

---
+++ src/sysexits.rs
@@ -0,0 +1,6 @@
+//! A collection of partially standard exit codes from C's `sysexits.h`. The actual utility of this is likely very low.
+#![allow(dead_code)]
+
+pub const USAGE: i32 = 64;
+pub const DATA_ERR:i32 = 65;
+// TODO: Complete this
