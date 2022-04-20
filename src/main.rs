739c22d17af34a63ffd491cbcc7d191b9359ae74
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2022-04-19 18:36:35 -0700

    added rule 110, demonstrating turing completeness

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,17 @@ use parse::parse;
 use std::fs;
 
 fn main() {
-    let source = fs::read_to_string("examples/list.bli").unwrap();
+    let source = fs::read_to_string("examples/rule110.bli").unwrap();
     let ast = parse(&source);
-    println!("AST ->\n    {:?}", ast);
+    //println!("AST ->\n    {:?}", ast);
     let (bytecode, idents) = compile(ast.unwrap());
-    println!("BYTECODE ->\n    {:?}", bytecode);
+    //println!("BYTECODE ->\n    {:?}", bytecode);
     let mut exec = Executor::from_code(bytecode, idents);
     exec.initialize_builtins();
-    println!("INITIAL EXECUTOR ->\n    {:?}", exec);
+    //println!("INITIAL EXECUTOR ->\n    {:?}", exec);
     println!("\n=== OUTPUT ===");
     let result = exec.run();
     println!("==============\n");
-    println!("FINISHED EXECUTOR ->\n    {:?}", exec);
+    //println!("FINISHED EXECUTOR ->\n    {:?}", exec);
     println!("RESULT ->\n    {:?}", result);
 }
