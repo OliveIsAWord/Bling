23e2be6473b13ec453f9f16b470ec07ee8a9fc90
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2022-04-20 13:06:56 -0700

    Demonstrated mathematical functions

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use parse::parse;
 use std::fs;
 
 fn main() {
-    let source = fs::read_to_string("examples/hello_world.bli").unwrap();
+    let source = fs::read_to_string("examples/maths.bli").unwrap();
     let ast = parse(&source);
     //println!("AST ->\n    {:?}", ast);
     let (bytecode, idents) = compile(ast.unwrap());
