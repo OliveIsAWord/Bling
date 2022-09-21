bf7f5be9f6cea6f4b3945fcb1f03c32bee68414b
Olive <89336634+OliveIsAWord@users.noreply.github.com>
2022-09-20 20:57:28 -0700

    🦀 And the cycle begins anew 🦀

--- improvements.md
+++ improvements.md
@@ -1,10 +1,4 @@
-Bling has made significant improvements from its estranged mother language Naglang.
+Bling 0.2 will make many improvements from 0.1:
 
-- Created with Rust rather than C :3
-- Function calls work on more than just identifiers, and function calls can be chained (e.g. `func(arg1)(arg2)(arg3)`)
-- Actual defined semantics
-- Honest to goodness bytecode and interpreter
-
-Further improvements that hopefully will be made in future work on the language:
-- IR optimizations
-- WASM as a compilation target
+- Keywords such as `if` and `while` to reduce verbosity.
+- An improved MIR to allow optimizations.
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,5 @@
-//! An interpreter for the Bling programming language.
-
-#![forbid(unsafe_code)]
-
-mod compile;
-mod interpret;
 mod parse;
-mod sysexits;
-
-use compile::compile;
-use interpret::Executor;
-use parse::parse;
-
-use std::env;
-use std::fs;
-use std::time::Instant;
-use std::process::exit;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let fp = args.get(1).unwrap_or_else(|| {
-        // identity closure necessary for converting &String to &str
-        let app_name = args.get(0).map_or("bling", |x| x);
-        eprintln!("Error: no source file specified");
-        eprintln!("Usage: {} <source file>", app_name);
-        exit(sysexits::USAGE);
-    });
-    let source = fs::read_to_string(fp).unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        exit(sysexits::NO_INPUT);
-    });
-    let ast = parse(&source).unwrap_or_else(|e| {
-        eprintln!("{}", e);
-        exit(sysexits::DATA_ERR);
-    });
-    //println!("AST ->\n    {:?}", ast);
-    let (bytecode, idents) = compile(ast);
-    //println!("BYTECODE ->\n    {:?}", bytecode);
-    let mut exec = Executor::from_code(bytecode, idents);
-    exec.initialize_builtins();
-    //println!("INITIAL EXECUTOR ->\n    {:?}", exec);
-    println!("\n=== OUTPUT ===");
-    let start_time = Instant::now();
-    let result = exec.run();
-    let total_time = start_time.elapsed();
-    println!("==============\n");
-    //println!("FINISHED EXECUTOR ->\n    {:?}", exec);
-    println!("RESULT ->\n    {:?}", result);
-    println!("Time Taken: {}μs", total_time.as_micros());
+    println!("Hello, Bling 0.2!");
 }
